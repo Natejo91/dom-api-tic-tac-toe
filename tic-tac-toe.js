b5da3d4efb36d7e83043bbb866418056705f8078
nathaniel <nathanieljortega@gmail.com>
2021-01-08 12:42:01 -0600

    Added some basic variables

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+window.addEventListener('DOMConentLoaded', event => {
+    let board = document.getElementById('tic-tac-toe-board');
+
+    const currentPlayerSymbol = 'x';
+    const squareValues = ['', '', '', '', '', '', '', '', ''];
+
+    board.addEventListener('click', event => {
+        if (event.target.id.includes('square-')) {
+
+        }
+    })
+
+
+});
